7f4cb657ae4a10f12c28a943cfef23f3c1db5490
jazmin.lombardi <jlombardidibetto@udesa.edu.ar>
2023-11-06 15:49:59 -0300

    creando

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuoxJkrJTwzA8AP10Hw6sdmcUZw2UAMwA",
+    authDomain: "proyecto3-6fbda.firebaseapp.com",
+    projectId: "proyecto3-6fbda",
+    storageBucket: "proyecto3-6fbda.appspot.com",
+    messagingSenderId: "903904162562",
+    appId: "1:903904162562:web:8dbc73939ee09f1851481b"
+  };
+  
