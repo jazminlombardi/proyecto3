f1436c035b3deccfbbda40d6e860c1a27298d1e4
luciacupito <lcupito@udesa.edu.ar>
2023-11-12 16:43:52 -0300

    cambios

--- App.js
+++ App.js
@@ -1,27 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { auth } from './src/firebase/config';
-import Register from './src/screens/Register/Register';
+ import { NavigationContainer } from '@react-navigation/native';
+ import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+ import Register from './src/screens/Register/Register';
+ import Login from './src/screens/Login/Login';
+ import Home from './src/screens/Home/Home';
+
+ const Stack = createNativeStackNavigator();
+
+ export default function App() {
+   // El control de sesión debería ser en app.js. Para ello tendriamos que transformarlo en componente con estado y chequear sesión en componentDidMount.
+
+   //Para mejorar la experiencia de usuario podemos usar un loader mientras chequeamos sesión.
 
 
-export default function App() {
-   //Registrar un Usuario
-   auth.createUserWithEmailAndPassword(email, pass)
-   //Loguear un Usuario
-   auth.signInWithEmailAndPassword(email, pass)
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+   return (
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+       <NavigationContainer style={styles.container}>
+         <Stack.Navigator>
+           <Stack.Screen name='Registro' component={Register} options={ { headerShown: false } }/>
+           <Stack.Screen name='Login' component={Login} options={ { headerShown: false } }/>
+           <Stack.Screen name='Home' component={Home} options={ { headerShown: false } }/>
+           {/* Si implementamos tabnavigation para el resto de la app. El tercer componente debe ser una navegación que tenga a Home como primer screen */}
+         </Stack.Navigator>
+       </NavigationContainer>
+
+   );
+ }
+
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: '#fff',
+     alignItems: 'center',
+     justifyContent: 'flex-start',
+   },
+ });
+
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,41 +1,70 @@
 import react, { Component } from 'react';
-import {TextInput, TouchableOpacity, View, Text, StyleSheet} from 'react-native';
-import { auth } from '../../firebase/config';
+ import {TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList} from 'react-native';
+ import { db, auth } from '../../firebase/config';
+ import PostForm from '../PostForm/PostForm';
+ import Post from '../../components/Post/Post';
 
-class Home extends Component {
-    constructor(){
-        super()
-        this.state={
-      
-        }
-    }
+ class Home extends Component {
+     constructor(){
+         super()
+         this.state={
+             posts:[]
+         }
+     }
 
-    logout(){
-        auth.signOut();
-         //Redirigir al usuario a la home del sitio.
-        // this.props.navigation.navigate('Login')
-    }
+     componentDidMount(){
+         //Traer los datos de Firebase y cargarlos en el estado.
+         db.collection('posts').onSnapshot(
+             listaPosts => {
+                 let postsAMostrar = [];
 
+                 listaPosts.forEach(unPost => {
+                     postsAMostrar.push({
+                         id:unPost.id,
+                         datos: unPost.data()            
+                     })
+                 })
 
+                 this.setState({
+                     posts:postsAMostrar
+                 })
+             }
+         )
 
-    render(){
-        return(
-            <View>
-                <Text>HOME</Text>
-                <TouchableOpacity onPress={()=>this.logout()}>
-                    <Text>Logout</Text>
-                </TouchableOpacity>
-               
-                <Text>//Importar form para crear Post</Text>
-                
-
-                <Text>//Agrear FlatList para mostrar los posteos creados</Text>
-
-            </View>
-        )
-    }
-}
+     }
 
 
+     logout(){
+         auth.signOut();
+          //Redirigir al usuario a la home del sitio.
+         // this.props.navigation.navigate('Login')
+     }
 
-export default Home;
+     render(){
+         console.log(this.state);
+         return(
+             <View>
+                 <Text>HOME</Text>
+                 <TouchableOpacity onPress={()=>this.logout()}>
+                     <Text>Logout</Text>
+                 </TouchableOpacity>
+
+                 <Text>Crear nuevo post</Text>
+                 <PostForm />
+
+                 <Text>Lista de posteos creados</Text>
+
+                 <FlatList
+                     data={this.state.posts}
+                     keyExtractor={ unPost => unPost.id }
+                     renderItem={ ({item}) => <Post dataPost = {item} />  }
+                 />
+
+             </View>
+         )
+     }
+ }
+
+
+
+ export default Home;
