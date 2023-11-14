b8d1d67f887b71c27efe82ec9a766b40471b2c61
luciacupito <lcupito@udesa.edu.ar>
2023-11-14 19:04:35 -0300

    cambios

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,4 +1,4 @@
-import react, { Component } from 'react';
+ import react, { Component } from 'react';
  import {TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList, Image} from 'react-native';
  import { db, auth } from '../../firebase/config';
  import PostForm from '../PostForm/PostForm';
